419072b199453ec5b16b7e306c72bfb50fe7bf19
harshilshah611 <harshilshah611@gmail.com>
2017-06-29 14:50:46 -0700

    Add some cool changes

--- src/app.py
+++ src/app.py
@@ -132,7 +132,7 @@ def fb_webhook():
             requests.post(request_url,
                           headers={'Content-Type': 'application/json'},
                           json={'recipient': {'id': sender_id},
-                                'message': {'text': "why"}})
+                                'message': {'text': "why qwerty"}})
 
     # Return an empty response.
     return ''
