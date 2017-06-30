153a08e28a230e759ae568d47cf1eea44d5adaaf
Harshil Shah <harshilshah611@gmail.com>
2017-06-29 19:22:25 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -28,6 +28,7 @@ app.config['FACEBOOK_WEBHOOK_VERIFY_TOKEN'] = 'mysecretverifytoken'
 
 
 db = SQLAlchemy(app)
+db.drop_all()
 
 senders = {}
 
@@ -61,7 +62,7 @@ class Todo(db.Model):
         self.data = data
         self.status = status
 
-db.drop_all()
+
 db.create_all()
         
 @app.route('/')
