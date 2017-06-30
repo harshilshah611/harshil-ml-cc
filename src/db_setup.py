f150f5b76dd5684ab30538db8dae19f905873bd1
Harshil Shah <harshilshah611@gmail.com>
2017-06-30 12:57:01 -0700

    Update db_setup.py

--- src/db_setup.py
+++ src/db_setup.py
@@ -1,9 +1,9 @@
-from app import TodoList,Address, User, db
+from app import Todo,Address, User, db
 
 db.drop_all()
 db.create_all()
 
-testList = TodoList(senderId = "a" ,listId = 1 ,data = "c" ,status = "N")
+testList = Todo(senderId = "a" ,listId = 1 ,data = "c" ,status = "N")
 admin = User(username='admin')
 guest = User(username='guest')
 admin_address = Address(full_address='1 Market St, San Francisco CA 94105',
@@ -13,8 +13,9 @@ guest_address = Address(
     user=guest)
 db.session.add(admin)  # This will also add admin_address to the session.
 db.session.add(guest)  # This will also add guest_address to the session.
+db.session.add(testList)
 db.session.commit()
 
 print User.query.all()
 print Address.query.all()
-print TodoList.query.all()
+print Todo.query.all()
